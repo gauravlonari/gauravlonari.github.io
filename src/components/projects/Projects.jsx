6e5bb2b777dd4bcd7d958deb1f08381da1df616e
Gaurav Lonari <gauravlonariofficial@gmail.com>
2022-10-08 18:15:04 +0530

    Basic Complete

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './projects.css'
-
+import elar from '../../assets/elar.png'
 export default function Projects() {
   return (
     <section id="projects">
@@ -8,10 +8,46 @@ export default function Projects() {
       <h2>Projects</h2>
       <div className="container projects_container">
         <article className='project_item'>
-          <div className="portfolio_item_image"></div>
-          <h3>title</h3>
-          <a href="https://github.com/gauravlonari" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
-          <a href="https://github.com/gauravlonari" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+          <div className="portfolio_item_image"><img src={elar} alt="elar" /></div>
+          <h3>ELAR</h3>
+          <a href="https://github.com/gauravlonari/ELAR" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
+          <a href="https://github.com/gauravlonari/ELAR/releases/tag/ELAR-APK" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='project_item'>
+          <div className="portfolio_item_image"><img src={elar} alt="elar" /></div>
+          <h3>Minibis</h3>
+          <a href="https://github.com/gauravlonari/NewNews" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
+          <a href="https://github.com/gauravlonari/ELAR/releases/tag/ELAR-APK" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='project_item'>
+          <div className="portfolio_item_image"><img src={elar} alt="elar" /></div>
+          <h3>Sahyadri Safar</h3>
+          <a href="https://github.com/gauravlonari/SahyadriSafar" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
+          <a href="https://github.com/gauravlonari/ELAR/releases/tag/ELAR-APK" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='project_item'>
+          <div className="portfolio_item_image"><img src={elar} alt="elar" /></div>
+          <h3>TextUtility</h3>
+          <a href="https://github.com/gauravlonari/TextUtility" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
+          <a href="https://github.com/gauravlonari/ELAR/releases/tag/ELAR-APK" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='project_item'>
+          <div className="portfolio_item_image"><img src={elar} alt="elar" /></div>
+          <h3>NewNews</h3>
+          <a href="https://github.com/gauravlonari/NewNews" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
+          <a href="https://github.com/gauravlonari/ELAR/releases/tag/ELAR-APK" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='project_item'>
+          <div className="portfolio_item_image"><img src={elar} alt="elar" /></div>
+          <h3>Stock Edge</h3>
+          <a href="https://github.com/gauravlonari/Stock-Edge" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
+          <a href="https://github.com/gauravlonari/ELAR/releases/tag/ELAR-APK" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+        </article>
+        <article className='project_item'>
+          <div className="portfolio_item_image"><img src={elar} alt="elar" /></div>
+          <h3>NotesKeeper</h3>
+          <a href="https://github.com/gauravlonari/NotesKeeper" target="_blank" rel='noreferrer' className='btn'>Source Code</a>
+          <a href="https://github.com/gauravlonari/ELAR/releases/tag/ELAR-APK" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
         </article>
       </div>
     </section>
