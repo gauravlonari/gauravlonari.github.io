4b7bcd156ea7bc4ba01c10d7ea0a27b790cad6bf
Gaurav Lonari <gauravlonariofficial@gmail.com>
2023-04-09 23:59:13 +0530

    Added react-snap

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import App from './App';
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
